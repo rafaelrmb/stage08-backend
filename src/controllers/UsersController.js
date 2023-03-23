aacf851ce0448cd7cb623083160fe96586883408
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-03-23 13:55:33 -0300

    add users controller

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,13 @@
+const { hash } = require("bcrypt");
+var moment = require('moment-timezone');
+
+class UsersController {
+  create(req, res) {
+    const { name, email, password } = req.body;
+
+    res.status(201).json({ name, email, password });
+  }
+};
+
+
+module.exports = UsersController;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,8 +1,8 @@
 const { Router } = require("express");
+const UsersController = require("../controllers/UsersController");
 const usersRoutes = Router();
+const usersController = new UsersController();
 
-usersRoutes.get("/");
-
-usersRoutes.post("/");
+usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
