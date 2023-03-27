5965ca2fd20b8380c6c4035027b54aee69552996
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-03-27 12:02:54 -0300

    add tags router and controller. index working

---
+++ src/controllers/TagsController.js
@@ -0,0 +1,18 @@
+const knex = require('../database/knex');
+
+
+class TagsController {
+  async index(req, res) {
+    const { user_id } = req.params;
+
+    const tags = await knex('tags').where({ user_id });
+
+    if (tags.length === 0) {
+      return res.status(404).json({ message: 'No tags were found for this user' });
+    }
+
+    return res.status(200).json(tags);
+  }
+}
+
+module.exports = TagsController;
