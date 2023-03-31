cc7392f68d60d0f33f210a5d787b7f8946288993
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-03-31 14:45:52 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const routes = require('./routes')
+const uploadConfig = require('./configs/upload');
+const cors = require('cors');
 
 const app = express();
+app.use(cors);
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.UPLOADS_FOLDER));
 app.use(routes);
 
 app.listen(PORT, () => {
